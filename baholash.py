8e82b80705d8e2cd882ac44219cb41171f73dd7b
Shahzod Rahmon <30922251+MrShahzodbek@users.noreply.github.com>
2018-06-29 16:22:19 +0500

    O'zgartirishlar kiritildi.

--- baholash.py
+++ baholash.py
@@ -2,21 +2,24 @@ Tanlov ishtirokchilari tomonidan taqdim etilgan ishlarning baholash tizimi
 
 1. Har bir ish hakamlar tomonidan 100 ballik tizimda baholanadi.
 2. Har bir hakam o'z bahosini qo'yadi. Yakuniy baho o'rtacha arifmetik asosida aniqlanadi.
-3. Baholash dizayn, struktura va xavfsizlik bo'yicha amalga oshiriladi.
+3. Baholash quyidagi kriteriyalar bo'yicha amalga oshiriladi. 
+ 3.1 Dizayn: Web 10 ball, mobil 5 ball, Web+Mobil 15 ball.
+ 3.2 Saytning funksionalligi 30 ball
+ 3.3 Saytning xavfsizligi 30 ball
+ 3.4 Kunlik foydalanuvchilar 5 ball dan boshlanadi.
+ 3.5 Saytning bazasi yani yo`nalishiga qarab uning har kunlik yangiliklar bazasi 10 ball
+ 3.6 Foydalanish va ro`yxatdan o`tish qulayligi 25 ball
+ 3.7 Yangiliklarni sayt foydalanuvchilariga elektron tarzda yetkazib borish imkoniyati 25 ball
+ 3.8 Kontentning unikalligi 15 ball
+ 3.9 Admin va ma'muriyatning bilimi 50 ball
 
-4. Strukturani baholash 40 ball
-4.1. Sayt bo'limlarining mavjudligi
-4.2. Lorem ipsum
-4.3. doler amit
-4.4. ipsum dolerim
-
-5. Dizaynni baholash 40 ball
-5.1. Logoning mavjudligi
-5.2. Dizaynning moslashuvchanligi
-5.3. Bootstrap elementlaridan foydalanish
-5.4. Lorep amibitdur ispum doler amit
-
-6. Xavfsizlik 20 ball
-6.1. Lorem ipsum
-6.2. Doler amei
-
+Qo`shimcha baholash:
+  - Saytda AJAX JS kodlardan mohirona foydalanish 20 ball
+  - Saytning qidiruv tizimlaridagi o`rni sayt titlesiga qarab qidirib ko`riladi va nechanchi sahifada chiqishiga qarab baholanadi
+  1-sahifa uchun 15 ball. Keyingi sahifalar uchun 3 balldan kamayib boradi.
+ 
+Saytdagi kamchiliklar uchun jarima ballar ayirib tashlanadi:
+1) Sayt dizaynidagi html xatolar yoki css xatolar uchun - 15 ball 
+2) Sayt himoyasidagi xatolik uchun - 50 ball
+3) Saytdan foydalanish pullik yoki majburiy reklamalarga bosish shartlari bo`lsa yoki shunga o`xshash narsalar - 50 ball
+4) Saytning o`z bazasi bo`la turib lekin ma`lumotlarni boshqa .uz saytlardan ko`chirib tashlash manba nomini o`rnatmaslik - 35 ball
