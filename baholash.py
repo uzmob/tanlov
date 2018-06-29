de2744836fa0be015a0ec5a4b717af7e93dc265e
Shahzod Rahmon <30922251+MrShahzodbek@users.noreply.github.com>
2018-06-29 16:37:58 +0500

    Update baholash.py

--- baholash.py
+++ baholash.py
@@ -4,22 +4,25 @@ Tanlov ishtirokchilari tomonidan taqdim etilgan ishlarning baholash tizimi
 2. Har bir hakam o'z bahosini qo'yadi. Yakuniy baho o'rtacha arifmetik asosida aniqlanadi.
 3. Baholash quyidagi kriteriyalar bo'yicha amalga oshiriladi. 
  3.1 Dizayn: Web 10 ball, mobil 5 ball, Web+Mobil 15 ball.
- 3.2 Saytning funksionalligi 30 ball
- 3.3 Saytning xavfsizligi 30 ball
- 3.4 Kunlik foydalanuvchilar 5 ball dan boshlanadi.
- 3.5 Saytning bazasi yani yo`nalishiga qarab uning har kunlik yangiliklar bazasi 10 ball
- 3.6 Foydalanish va ro`yxatdan o`tish qulayligi 25 ball
- 3.7 Yangiliklarni sayt foydalanuvchilariga elektron tarzda yetkazib borish imkoniyati 25 ball
- 3.8 Kontentning unikalligi 15 ball
- 3.9 Admin va ma'muriyatning bilimi 50 ball
+ 3.2 Saytning funksionalligi 15 ball
+ 3.3 Saytning xavfsizligi 15 ball
+ 3.4 Kunlik foydalanuvchilar 5 ball.
+ 3.5 Saytning kunlik yangilanishlari 10 ball
+ 3.6 Foydalanish va ro`yxatdan o`tish qulayligi 10 ball
+ 3.7 Yangiliklardan xabardor qilish imkoniyati 10 ball
+ 3.8 Kontentning unikalligi 10 ball
+ 3.9 Admin bilimi 10 ball
 
-Qo`shimcha baholash:
-  - Saytda AJAX JS kodlardan mohirona foydalanish 20 ball
-  - Saytning qidiruv tizimlaridagi o`rni sayt titlesiga qarab qidirib ko`riladi va nechanchi sahifada chiqishiga qarab baholanadi
-  1-sahifa uchun 15 ball. Keyingi sahifalar uchun 3 balldan kamayib boradi.
+4. Qo`shimcha baholash [bonus ballar]:
+  4.1 Saytda AJAX JS kodlardan mohirona foydalanish 20 ball
+  4.2 Saytning qidiruv tizimlaridagi o`rni sayt titlesiga qarab qidirib ko`riladi va nechanchi sahifada chiqishiga qarab baholanadi
+     1-sahifa uchun 15 ball. Keyingi sahifalar uchun 3 balldan kamayib boradi.
+  4.3 Alexa Indexdagi natijalar:
+     >1000000 = 0 ball, 100000-999999 = 5 ball, <99999 = 10 ball, <1000 = 15 ball
  
-Saytdagi kamchiliklar uchun jarima ballar ayirib tashlanadi:
-1) Sayt dizaynidagi html xatolar yoki css xatolar uchun - 15 ball 
-2) Sayt himoyasidagi xatolik uchun - 50 ball
-3) Saytdan foydalanish pullik yoki majburiy reklamalarga bosish shartlari bo`lsa yoki shunga o`xshash narsalar - 50 ball
-4) Saytning o`z bazasi bo`la turib lekin ma`lumotlarni boshqa .uz saytlardan ko`chirib tashlash manba nomini o`rnatmaslik - 35 ball
+5. Saytdagi kamchiliklar uchun jarima ballar ayirib tashlanadi:
+ 5.1 Sayt dizaynidagi html xatolar yoki css xatolar uchun
+     1-5 ta xato 5 ball, 6-10 ta xato 10 ball, 10+ ta xato 15 ball,  
+ 5.2 Sayt himoyasidagi xatolik uchun  20 ball
+ 5.3 Foydalanish pullik yoki majburiy reklamalarga bosish shartligi 20 ball
+ 5.4 Ko`chirmakashlik, mualliflik huquqining buzulishi 15 ball
