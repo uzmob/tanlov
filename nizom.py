0e6cee8c9d2f2b3b3604cbe2315094531520331f
Shahzod Rahmon <30922251+MrShahzodbek@users.noreply.github.com>
2018-07-09 17:38:36 +0500

    Create nizom.py

---
+++ nizom.py
@@ -0,0 +1,161 @@
+
+Tanlovni o'tkazish nizomi
+
+I. Asosiy maqsad va vazifalar
+
+Tanlovni o‘tkazishdan ko‘zlangan asosiy maqsad va vazifalar quyidagilardan iborat:
+
+Milliy segmentni Respublikamizda ishlab chiqarilgan axborot resurslari va tizimlari bilan boyitish;
+
+Yaratilayotgan veb-saytlar va dasturiy maxsulotlar sifatini hamda ulardagi axborot xavfsizligini oshirish;
+
+Kelgusida saralab olingan dasturchilarni qo‘llab quvvatlash va ularning qobiliyatlarini baxolash;
+
+Zamonaviy Internet texnologiyalarini jamiyatning turli sohalariga - davlat boshqaruvi, ta’lim, madaniyat va biznes sohalariga tatbiq etilishini rag‘batlantirish.
+
+II. Tanlovni tashkil etish va o‘tkazish tartibi
+
+2.1.Tanlov tashkiliy qo‘mitasi tarkibi Uzfor.uz ma'muriyati tomonidan tasdiqlanadi.
+
+2.2.Tanlovda soha mutaxassislaridan iborat hakamlar hay’ati "Tashkiliy qo‘mita rahbari” tomonidan tasdiqlanadi.
+
+2.3.Tanlov uch bosqich (tanlov ishtirokchilarini ro‘yxatga olish, g‘olib va sovrindorlarni saralash hamda taqdirlash)da o‘tkaziladi.
+
+2.4.Tanlovda Uzfor.uz forumidan ro‘yxatdan o‘tgan barcha jismoniy/yuridik shaxslar ishtirok etishi mumkin.
+
+2.5.Tanlov ishtirokchilari arizalari uzfor.uz sayti orqali 2018-yilning 10-iyulidan 2018-yil 13-iyuliga qadar qabul qilinadi.
+
+2.6.Tanlovga Internet tarmog‘ida joylashtirilgan saytlar taqdim etilishi mumkin. Bitta ishtirokchi tomonidan tanlovga ko‘pi bilan ikkitagacha ishlanmalar taqdim etilishi mumkin.
+
+2.7.Tanlovga saytning mulkdori (jismoniy yoki yuridik shaxs) – domenning (qonuniy) egasi roziligi asosida sayt yaratuvchisi tomonidan ariza qabul qilinadi.
+
+2.8.Tanlovda 7 yoshdan 70 yoshgacha bo‘lganlar ishtirok etishlari mumkin.
+
+2.9.Tashkiliy qo‘mita tanlov ishtirokchilariga kerak bo‘ladigan barcha ma’lumotlarni joylashtiradi.
+
+2.10.Tanlovda ishtirok etuvchi ishlanmaning mazmuniga quyiladigan talablar:
+
+- ishlanmaning kontenti - tanlov auditoriyasi (istisno tariqasida barcha yoshdagilar) uchun mos kelishi lozim;
+
+- ishlanmaning kontenti- yoshlarni barkamol shaxs etib tarbiyalashga yo‘naltirilgan bo‘lishi, ularning bilim darajasini va dunyoqarashini oshirishi kerak;
+
+- ishlanmaning kontenti - internet foydalanuvchisida axborot olish madaniyatini hamda undan xavfsiz foydalanish ko‘nikmalarini shakllantirishga xizmat qilishi kerak;
+
+- ishlanmadagi axborotlarning kamida 80 foizi kontent, rukn va sahifalarning hajmiga qarab belgilanadi, foydali kontent bilan to‘ldirilgan bo‘lishi, doimiy ravishda yangi materiallar bilan boyitib borilishi kerak;
+
+- ishlanmaning interfaol ruknlari (yangiliklar, voqea-hodisalar) doimiy ravishda (haftasida kamida bir marotaba) yangilanib turilishi kerak;
+
+- tanlovda ishtirok etayotgan ishlanmaga uchinchi shaxslarning huquqlari, shu jumladan, mualliflik huquqini poymol etuvchi materiallar, milliy qonunchilikka zid bo‘lgan materiallar, ma’naviy va axloqiy me’yorlarga to‘g‘ri kelmaydigan materiallar, xavfsizlik talablariga javob bermaydigan, shuningdek, O‘zbekiston Respublikasi hududida tarqatilishi ta’qiqlangan mahsulot va xizmatlar reklamasini joylashtirish ta’qiqlanadi;
+
+- Tanlovda ishtirok etayotgan ishlanma vizual bezatishning zamonaviy talablari (yuqori sifatli grafika, shrift va ranglarning mutanosibligi, uni mazmuni va auditoriyasiga mos format va boshqalar)ga javob berishi lozim;
+
+Hakamlar hay’ati tomonidan "UZ” domeni hamda fizik jihatdan milliy segmentda joylashtirilgan veb resurslarga alohida e’tibor qaratiladi.
+
+III. Tanlovni o‘tkazish muddatlari
+
+Tanlov uch bosqichda o‘tkaziladi:
+
+1-bosqich: 10-iyuldan 13-iyulgacha – nominatsiyalar bo‘yicha tanlov ishtirokchilarini ro‘yxatga olish va ma’lumotlar bazasini shakllantirish;
+
+2-bosqich: tanlov g‘olibi va sovrindorlarini saralash 30-iyulda o‘tkaziladi;
+
+3-bosqich: Tanlov g‘olib va sovrindorlarini taqdirlash marosimi 2018-yil 10-avgust kuni o‘tkaziladi.
+
+ 
+
+IV.Tashkiliy qo‘mita hamda ishtirokchilarning huquq va majburiyatlari
+
+4.1.Tashkiliy qo‘mita faoliyati davomida quyidagi vazifalarni amalga oshiradi:
+
+- mazkur Nizom talablari doirasida tanlov o‘tkazish bilan bog‘liq tadbirlarni amalga oshiradi;
+
+- tanlovning o‘tkazilish shartlari (tanlovni o‘tkazish qoidalari, baholash jarayoni, mezonlari)ni belgilaydi;
+
+- hakamlar hay’atining tarkibi hamda ularning rahbarlarini tayinlaydi;
+
+- tanlovga arizalar qabul qilish jarayonini tashkil etadi, arizalarning mazkur Nizomda ko‘rsatilgan talablar asosida rasmiylashtirilishini nazorat qiladi;
+
+- tanlovni o‘tkazish jarayonida hakamlar hay’atining ishini muvofiqlashtirib boradi;
+
+- tanlov g‘oliblari va sovrindorlarini taqdirlash marosimini tashkil etadi;
+
+- tanlovning boshqa tashkiliy masalalarini hal etadi.
+
+ 
+
+4.2.Tashkiliy qo‘mita o‘z faoliyati davomida quyidagi huquqlarga ega:
+
+- tanlovga taqdim etilgan ishlanma mazkur Nizomda ko‘rsatilgan talablarga javob bermagan taqdirda nomzodning arizasini qaytarish;
+
+- tanlov ishtirokchisi mazkur Nizomda ko‘rsatilgan talablarni buzgan hamda tanlovning o‘tkazilish talab va shartlariga mos kelmagan taqdirda uni tanlovdan chetlashtirish;
+
+- Hakamlar hay’ati tomonidan ularning vakolati doirasida to‘g‘rilab bo‘lmaydigan texnik xatolar aniqlangan nominatsiyalar bo‘yicha tanlov natijalarini bekor qilish.
+
+4.3.Tanlov ishtirokchisining huquqlari quyidagilardan iborat:
+
+- tanlovni o‘tkazish shartlari va tartibi to‘g‘risida ma’lumot olish;
+
+- tanlovda ishtirok etish uchun belgilangan muddatda ariza berish;
+
+- Tashkiliy qo‘mitaga arizalar qabul qilish muddati tugashidan kamida 2 kun oldin rasmiy murojaat etgan holda o‘z arizasini qaytarib olish;
+
+- tanlov g‘olibi deb e’lon qilinganida sovrinlarni o‘zi yoki ishonchli vakili(ishonchnoma) orqali olish.
+
+ 
+
+4.4.Tanlov ishtirokchisining majburiyatlari quyidagilardan iborat:
+
+- arizani topshirishdan oldin tanlov to‘g‘risidagi mazkur Nizom bilan to‘liq tanishish;
+
+- mazkur Nizomda ko‘rsatilgan talablar asosida to‘ldirilgan arizani belgilangan muddatda taqdim etish;
+
+- mazkur Nizomda ko‘rsatilgan qoidalarga qat’iy rioya etish.
+
+V. Tanlov shartlari va baholash mezonlari
+
+5.1.Ishlanmalar (tanlov ishtirokchilarining ishlari) hakamlar hay’ati tomonidan quyidagi asosiy mezonlar bo‘yicha baholanadi:
+
+- Mazmundorlik va interfaollik. Ishlanmaning mazmuni bu – ishlanmada mavjud barcha axborotlar. Axborot foydalanuvchilar e’tiborini tortishi hamda uning mavzusiga mos kelishi lozim. Uning interfaolligi faqat giperhavolalar va o‘z-o‘zidan paydo bo‘luvchi menyularning mavjudligi bilan belgilanmaydi, balkim foydalanuvchiga o‘zaro axborot almashish uchun muloqot muhitini yaratishi kerak.
+
+- Dizayn va qulaylik. Ishlanma tashqi ko‘rinishining tavsifi. Ishlanmaning vizual bezatilishi, yuqori sifatga ega grafika, shriftlar va ranglarning aniq tanlanganligi va mutanosibligi, formati va mazmunining auditoriya hamda qo‘yilgan vazifalarga mosligiga qarab baholanadi. Qulaylik deganda ishlanmadan foydalanishdagi imkoniyatlar tushuniladi. Ishlanmada axborotdan foydalanish hamda bir bo‘limdan ikkinchisiga o‘tishdagi qulayliklar uning tarkibiy tuzilmasi va navigatsion funktsiyalariga bog‘liq. Ishlanmadan kerakli axborotlarni tezkor topishda navigatsion imkoniyatlarning kengligi.
+
+- Funktsionalligi, texnologik imkoniyatlari va axborot xavfsizligi. Ushbu mezon ishlanmaning texnologik xususiyatlarini ifodalaydi. Yaxshi funktsionallik ishlanmaning tez ochilishini, undagi barcha havolalar ishchi holatda bo‘lishini anglatadi. Texnologiyalar esa ishlanma auditoriyasining talablariga javob berishi kerak. Ishlanmaning sifati imkon qadar serverlik platformasi, brauzerning turi yoki foydalanuvchi monitoriga bog‘liq bo‘lmasligi lozim. Ishlanmada axborot resurslarining xavfsizligini ta’minlash maqsadida ishlanmadan foydalanishni cheklash (autentifikatsiya va avtorizatsiya bosqichlari) mavjud bo‘lishi kerak.
+
+- Tanlovda ishtirok etadigan saytlar O‘zbekistonning milliy segmentida joylashtirilganligi, fizik jihatdan "UZ” domeni zonasida bo‘lishi baholash mezonlarida hakamlar hay’ati tomonidan alohida e’tiborga olinadi.
+
+- Tanlovda ishtirok etadigan saytlar avvalo davlat tili (o‘zbek tili)da, qo‘shimcha tarzda boshqa xorijiy tillarda bo‘lishi ham mumkin.
+
+- Ishlanmalar (ishtirokchilarning ijodiy ishlari) hakamlar hay’ati tomonidan 100 ballik tizimda baholanadi.
+
+ 
+
+5.2.Tanlovning asosiy nominatsiyasi g‘olibi qo‘yidagi holatlarda aniqlanmasligi mumkin:
+
+- Tashkiliy qo‘mitaning maxsus qaroriga muvofiq muayyan nominatsiya (yoki qo‘shimcha nominatsiya) bo‘yicha tanlovga topshirilgan ishlarning hech biri talabga javob bermagan taqdirda;
+
+- Nominatsiya (yoki qo‘shimcha nominatsiya)da ishtirok etuvchilar soni
+3 tadan kam bo‘lsa hamda ular orasidan 1-o‘ringa munosib deb topilmagan taqdirda.
+
+VI. G‘oliblarni taqdirlash
+
+6.1.Tanlov g‘oliblarini taqdirlashni tashkil etish maqsadida tashkiliy qo‘mitaning majlisi o‘tkaziladi hamda tanlovning natijalari e’lon qilinadi. Tanlovning ikkinchi bosqichidan so‘ng hakamlar hay’ati tomonidan qo‘yilgan baholar hamda tanlov g‘oliblari va sovrindorlarining ro‘yxati tashkiliy qo‘mita tomonidan tasdiqlanadi.
+
+6.2.Tanlov g‘oliblari va sovrindorlarining nomlari tantanali taqdirlash marosimida e’lon qilinadi.
+
+6.3.Tanlov natijalari rasman e’lon qilingunga qadar hakamlar hay’ati hamda tashkiliy qo‘mita a’zolari bu haqdagi ma’lumotlarni oshkor etishlari mumkin emas.
+
+6.4.Tanlov natijalari va g‘oliblarning ro‘yxati tantanali taqdirlash marosimidan so‘ng tanlovning rasmiy sayti hamda axborot hamkorlari hisoblangan ommaviy axborot vositalarida e’lon qilinadi.
+
+6.5.Tanlov g‘oliblari diplom va esdalik sovg‘alar bilan taqdirlanadi.
+
+6.6.Tashkiliy qo‘mita tanlov doirasida internet va bevosita tanlovning rivojiga munosib hissa qo‘shgan ishlar yoki shaxslarni taqdirlash huquqiga egadir.
+
+VII. Tanlov nominatsiyalari
+
+Tanlov 3 ta nominatsiyalardan iborat:
+
+1."Eng yaxshi ta’limga oid veb-sayt” (Turli mavzularga yo‘naltirilgan o‘quv-tarbiyaviy mazmundagi saytlar, jurnal va kitoblarning elektron nusxalari, oilalar yoki ota-onalar ehtiyojiga ko‘ra foydalanishga mo‘ljallangan, shuningdek o‘zbek adabiyotining buyuk adiblari hayoti va ijodlari yoritilgan veb-saytlar);
+
+2."Eng yaxshi tadbirkor veb-sayti” (Kichik biznes va xususiy tadbirkorlik faoliyatini yoritishga va ishlab chiqarayotgan yoki sotayotgan maxsulotlarini namoyish etishga qaratilgan veb-saytlar);
+
+3."Eng yaxshi davlat tashkiloti veb-sayti” (Viloyatimizdagi hokimliklar, boshqarma va tashkilotlarning o‘z faoliyatini yoritgan veb-saytlari). 
